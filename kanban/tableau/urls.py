791c43cfe3845b89af2bfa88fbfe2fad65d3cd6e
Pindice <pascal.indice@gmail.com>
2023-03-29 14:55:55 +0400

    front static v1

--- kanban/tableau/urls.py
+++ kanban/tableau/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from . import views
-from .views import kanban
-
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('kanban/', kanban, name='kanban')
+    path('colonnes/', views.get_colonnes, name='get_colonnes'),
+    path('colonnes/create/', views.create_colonne, name='create_colonne'),
+    path('colonnes/delete/<int:colonne_id>/', views.delete_colonne, name='delete_colonne'),
+    path('colonnes/update/<int:colonne_id>/', views.update_colonne, name='update_colonne'),
+    path('taches/create/', views.create_tache, name='create_tache'),
+    path('taches/delete/<int:tache_id>/', views.delete_tache, name='delete_tache'),
+    path('taches/update/<int:tache_id>/', views.update_tache, name='update_tache')
 ]
--- kanban/tableau/views.py
+++ kanban/tableau/views.py
@@ -1,63 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.http import JsonResponse
-from rest_framework import generics
 from .models import Colonne, Tache
-# Create your views here.
-
 
 
 def index(request):
-    return HttpResponse("Hello, world.")
-
-def kanban(request):
-    data = [
-        {
-            'id': 'board1',
-            'title': 'À faire',
-            'class': 'info',
-            'item': [
-                {
-                    'title': 'Tâche 1',
-                    'description': 'Description de la tâche 1'
-                },
-                {
-                    'title': 'Tâche 2',
-                    'description': 'Description de la tâche 2'
-                }
-            ]
-        },
-        {
-            'id': 'board2',
-            'title': 'En cours',
-            'class': 'warning',
-            'item': [
-                {
-                    'title': 'Tâche 3',
-                    'description': 'Description de la tâche 3'
-                }
-            ]
-        },
-        {
-            'id': 'board3',
-            'title': 'Terminé',
-            'class': 'success',
-            'item': [
-                {
-                    'title': 'Tâche 4',
-                    'description': 'Description de la tâche 4'
-                }
-            ]
-        }
-    ]
-    return JsonResponse(data, safe=False)
+    return render(request, 'tableau/index.html')
 
 def get_colonnes(request):
     colonnes = Colonne.objects.all().values()
     colonnes_list = list(colonnes)
     for colonne in colonnes_list:
         taches = Tache.objects.filter(colonne_id=colonne['id']).values()
-        colonne['cards'] = list(taches)
+        colonne['item'] = list(taches)
     return JsonResponse(colonnes_list, safe=False)
 
 def create_colonne(request):
